51db6888e72a224b3fe18a2d1c1a31d148c75112
Arthur Pimentel <arthurmoraispimentel7@gmail.com>
2022-05-19 14:17:12 -0300

    Declared Functions BenchMark

--- src/AgRunner.java
+++ src/AgRunner.java
@@ -2,23 +2,47 @@
 import ag.Ag;
 import individual.Individual;
 import individual.IndividualFactory;
-//import individualPerm.IndividualPermFactory;
+import individualPerm.IndividualPermFactory;
+import individualCrossInTray.IndividualCrossInTrayFactory;
 import individualSchwefel.IndividualSchwefelFactory;
 
 public class AgRunner {
     public static void main(String[] args) throws Exception {
         int nPop = 20;
         int dimension = 2;
-        int nGer = 2000;
+        int nGer = 10000;
         int nElite = 4;
         Double shift;
 
         /*
-         * IndividualFactory individualFactory = new IndividualPermFactory(dimension);
+         *
+         * Function Perm
+         *
+         */
+
+        shift = 0.0;
+
+        IndividualFactory individualFactory = new IndividualPermFactory(dimension);
+
+        Ag ag = new Ag();
+
+        Individual individual = ag.run(nPop, individualFactory, nElite, nGer, shift);
+
+        /*
+         *
+         * Function Cross In Tray
+         *
+         */
+
+        /*
+         * shift = -2.1;
+         * 
+         * IndividualFactory individualFactory = new
+         * IndividualCrossInTrayFactory(dimension);
          * 
          * Ag ag = new Ag();
          * 
-         * Individual individual = ag.run(nPop, individualFactory, nElite, nGer);
+         * Individual individual = ag.run(nPop, individualFactory, nElite, nGer, shift);
          */
 
         /*
@@ -26,13 +50,17 @@ public class AgRunner {
          * Function Schwefel
          *
          */
-        shift = 0.0;
 
-        IndividualFactory individualFactory = new IndividualSchwefelFactory(dimension);
-
-        Ag ag = new Ag();
-
-        Individual individual = ag.run(nPop, individualFactory, nElite, nGer, shift);
+        /*
+         * shift = 0.0;
+         * 
+         * IndividualFactory individualFactory = new
+         * IndividualSchwefelFactory(dimension);
+         * 
+         * Ag ag = new Ag();
+         * 
+         * Individual individual = ag.run(nPop, individualFactory, nElite, nGer, shift);
+         */
 
     }
 
